ba6e6011477293db4ed1487ebad65efe62576d99
issacreal <issacreal@gmail.com>
2017-05-28 23:14:01 +0800

    Modoulizing

--- Get.py
+++ Get.py
@@ -7,21 +7,31 @@ baseUrl = "http://www.tse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date=
 
 
 year   = 2010
-month  = 10
+month  = 9
 
-yy = str(year)
-mm = str(month)
 
-if(month < 10):
-    mm =  "0" + mm
+def GetData(year,month):
+    yy = str(year)
+    mm = str(month)
+    
+    if(month < 10):
+        mm =  "0" + mm
 
-targetUrl = baseUrl + yy + mm + "01"
-print(targetUrl)
+    targetUrl = baseUrl + yy + mm + "01"
+    print(targetUrl)
 
-res = requests.get(targetUrl)
-#print(res.text)
-tt = res.text
-j =  json.loads(tt)
+    res = requests.get(targetUrl)
+    #print(res.text)
+    data =  json.loads(res.text)["data"]
 
-for data in j["data"]:
-    print(data[0])
+    for e in data:
+        print(e)
+    
+    return data
+
+
+q = GetData(year,month)    
+
+month = 11
+
+q = q + GetData(year,month)      
