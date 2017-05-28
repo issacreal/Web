10425aede31ac9afdbb0fd966a2318265bc49add
issacreal <issacreal@gmail.com>
2017-05-28 22:44:48 +0800

    Decode JSON OK

--- Get.py
+++ Get.py
@@ -1,12 +1,13 @@
-import bs4
 import requests
 import json
 
 #targetUrl = "http://www.tse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date=19990515"
 baseUrl = "http://www.tse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date="
 
+
+
 year   = 2010
-month  = 2
+month  = 10
 
 yy = str(year)
 mm = str(month)
@@ -21,4 +22,6 @@ res = requests.get(targetUrl)
 #print(res.text)
 tt = res.text
 j =  json.loads(tt)
-print(j["data"][1])
+
+for data in j["data"]:
+    print(data[0])
