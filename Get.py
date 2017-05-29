bda38ab852f4c18251c54c9e16493e83aaa76154
issacreal <issacreal@gmail.com>
2017-05-29 14:11:12 +0800

    Pickle JSON data

--- Get.py
+++ Get.py
@@ -1,13 +1,16 @@
 import requests
 import json
+import pickle
 
 #targetUrl = "http://www.tse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date=19990515"
 baseUrl = "http://www.tse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date="
 
 
 
-year   = 2010
-month  = 9
+startYear   = 2010
+startMonth  = 9
+endYear     = 2017
+endMonth  = 5
 
 
 def GetData(year,month):
@@ -23,15 +26,27 @@ def GetData(year,month):
     res = requests.get(targetUrl)
     #print(res.text)
     data =  json.loads(res.text)["data"]
-
+   
+    '''
     for e in data:
         print(e)
-    
+    '''
     return data
 
+q = GetData(startYear,1)
+for year in range(startYear,endYear + 1):    
+    for month in range(1,12 + 1):
+        if(month < endMonth and year < endYear):
+            q = q + GetData(year,month)
 
-q = GetData(year,month)    
+with open('outfile.txt', 'wb+') as fp:
+   pickle.dump(q, fp)
 
-month = 11
+with open('outfile.txt', 'rb') as fp:
+    itemlist = pickle.load(fp)
+    
+#print(itemlist)    
 
-q = q + GetData(year,month)      
+for e in itemlist:
+    print(e)   
+    
